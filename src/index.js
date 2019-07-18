ab6ec9e9729d9620fd6794c5eb916ef85d75d2f0
Anonymous <anonymous@localhost.localdomain>
2019-07-18 19:16:28 +0100

    add vue js and vuetify to project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 //--------  Routers  ---------//
-var serverRouter=require('./src/routers/server');
+var serverRouter=require('./routers/main');
 
 
 
