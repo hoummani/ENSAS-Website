ce43c1f79e17c9fba5174c67e0d5e676019c2cdf
Anonymous <anonymous@localhost.localdomain>
2019-08-14 11:37:40 +0100

    begin to custumising the tabs component

--- admin/src/views/PFE.vue
+++ admin/src/views/PFE.vue
@@ -1,7 +1,45 @@
 <template>
   <div>
     <h5 class="subheading text-uppercase grey--text">Espace PFE</h5>
-    <div></div>
+    <div>
+      <v-card>
+        <v-tabs background-color="white" color="indigo" left>
+          <v-tab href="#tab-1">Entreprises</v-tab>
+          <v-tab href="#tab-2">Gestion des rapports</v-tab>
+          <v-tab href="#tab-3">Membres des jurys</v-tab>
+
+          <v-tab-item value="tab-1">
+            <v-container fluid>
+              <v-row>
+                <div class="ma-3">
+                  <p>Entreprise  Entreprise Entreprise Entreprise Entreprise Entreprise Entreprise Entreprise Entreprise</p>
+                </div>
+              </v-row>
+            </v-container>
+          </v-tab-item>
+
+          <v-tab-item value="tab-2">
+            <v-container fluid>
+              <v-row>
+                <div class="ma-3">
+                  <p>Rapport Rapport Rapport Rapport Rapport Rapport Rapport Rapport</p>
+                </div>
+              </v-row>
+            </v-container>
+          </v-tab-item>
+
+          <v-tab-item value="tab-3">
+            <v-container fluid>
+              <v-row>
+                <div class="ma-3">
+                  <p>Jury Jury</p>
+                </div>
+              </v-row>
+            </v-container>
+          </v-tab-item>
+        </v-tabs>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -9,9 +47,9 @@
 export default {
   data() {
     return {
-      
-    }
-  },
-}
+      tabs: 3
+    };
+  }
+};
 </script>
 
