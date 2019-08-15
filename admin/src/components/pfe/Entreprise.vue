e0d0b29839375c868b3479ad0aeadec64899fef7
Anonymous <anonymous@localhost.localdomain>
2019-08-15 12:18:53 +0100

    mixing add and edit form in one form

--- admin/src/components/pfe/Entreprise.vue
+++ admin/src/components/pfe/Entreprise.vue
@@ -42,27 +42,31 @@
                   <v-icon>arrow_downward</v-icon>
                 </v-btn>
               </v-btn-toggle>
-              <v-btn color="white" class="ml-1" @click="onAddItem()" fab icon>
-                <v-icon dark>add</v-icon>
-              </v-btn>
+              <!-- add item  -->
+
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-btn color="white" class="ml-1" @click="onAddItem()" fab icon v-on="on">
+                    <v-icon dark>add</v-icon>
+                  </v-btn>
+                </template>
+                <span class="caption text-lowercase">Ajouter Entreprise</span>
+              </v-tooltip>
+
+              <!-- end add item  -->
             </template>
           </v-toolbar>
         </template>
 
         <template v-slot:default="props">
           <v-row>
-            <v-col
-              v-for="item in props.items"
-              :key="item.email"
-              cols="12"
-              sm="6"
-              md="4"
-              lg="4"
-            >
+            <v-col v-for="item in props.items" :key="item.email" cols="12" sm="6" md="4" lg="4">
               <v-card>
-                <v-card-title class="subheading font-weight-thin">{{
+                <v-card-title class="subheading font-weight-thin">
+                  {{
                   item.nom
-                }}</v-card-title>
+                  }}
+                </v-card-title>
 
                 <v-divider></v-divider>
 
@@ -73,9 +77,11 @@
                     :color="sortBy === key ? `blue lighten-4` : `white`"
                   >
                     <v-list-item-content>{{ key }}:</v-list-item-content>
-                    <v-list-item-content class="caption align-start">{{
+                    <v-list-item-content class="caption align-start">
+                      {{
                       item[key.toLowerCase()]
-                    }}</v-list-item-content>
+                      }}
+                    </v-list-item-content>
                   </v-list-item>
                 </v-list>
                 <v-divider></v-divider>
@@ -123,9 +129,7 @@
 
             <v-spacer></v-spacer>
 
-            <span class="mr-4 grey--text"
-              >Page {{ page }} de {{ numberOfPages }}</span
-            >
+            <span class="mr-4 grey--text">Page {{ page }} de {{ numberOfPages }}</span>
             <v-btn fab text class="mr-1" @click="formerPage">
               <v-icon>keyboard_arrow_left</v-icon>
             </v-btn>
@@ -136,38 +140,76 @@
         </template>
       </v-data-iterator>
     </v-container>
-
+    <!--   ################ -->
     <!-- add entreprise  -->
     <v-layout justify-center>
-      <v-dialog v-model="addItemDialog" persistent max-width="590">
+      <v-dialog v-model="itemDialog" persistent max-width="590">
         <v-card>
-          <v-card-title class="headline">Ajouter Une Entreprise</v-card-title>
+          <v-card-title class="headline">{{itemFormTitle}}</v-card-title>
           <v-divider></v-divider>
           <v-card-text>
             <v-container grid-list-md>
-              <v-form
-                ref="addItemForm"
-                v-model="addItemValidForm"
-                lazy-validation
-              >
+              <v-form ref="itemForm" v-model="itemValidForm" lazy-validation>
                 <v-layout wrap>
                   <v-text-field
                     type="text"
                     class="mr-1"
+                    :rules="ItemObjectRules.textInput"
                     label="Nom"
+                    v-model="ItemObject.nom"
                     required
                   ></v-text-field>
 
                   <v-text-field
-                    type="text"
-                    label="Type"
+                    type="email"
+                    label="Email"
+                    :rules="ItemObjectRules.emailInput"
+                    v-model="ItemObject.email"
                     required
                   ></v-text-field>
+
+                  <v-select
+                    v-model="ItemObject.type"
+                    :items="['Privee','Public','Semi-Public']"
+                    :rules="ItemObjectRules.requiredField"
+                    label="Type"
+                    class="mr-1"
+                    :value="ItemObject.type"
+                  ></v-select>
 
                   <v-text-field
                     class="mr-1"
+                    type="phone"
+                    :rules="ItemObjectRules.phoneInput"
+                    v-model="ItemObject.telephone"
+                    label="Telephone"
+                    required
+                  ></v-text-field>
+
+                  <v-text-field
                     type="text"
-                    label="Email"
+                    class="mr-1"
+                    label="Specialitee"
+                    :rules="ItemObjectRules.textInput"
+                    v-model="ItemObject.specialitee"
+                    required
+                  ></v-text-field>
+
+                  <v-text-field
+                    type="text"
+                    class="mr-1"
+                    label="Adresse"
+                    :rules="ItemObjectRules.adresseInput"
+                    v-model="ItemObject.adresse"
+                    required
+                  ></v-text-field>
+
+                  <v-text-field
+                    type="number"
+                    class="mr-1"
+                    label="ZIPCode"
+                    :rules="ItemObjectRules.requiredField"
+                    v-model="ItemObject.zipcode"
                     required
                   ></v-text-field>
                 </v-layout>
@@ -178,7 +220,7 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="indigo darken-1" text @click="reset">Annuler</v-btn>
-            <v-btn color="indigo darken-1" text @click="submit">Modifier</v-btn>
+            <v-btn color="indigo darken-1" text @click="submit">Enregistrer</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -203,27 +245,38 @@ export default {
         "Telephone",
         "Type",
         "Specialitee",
-        "pay",
-        "Ville",
         "ZipCode",
-        "Rue"
+        "Adresse"
       ],
       //add item
-
-      addItemDialog: false,
-      addItemValidForm: false,
-      addItemObject: {
+      itemFormTitle:"Ajouter Une Entreprise",
+      onEdit:false,
+      itemDialog: false,
+      itemValidForm: false,
+      ItemObject: {
         nom: "",
 
         email: "",
 
         telephone: "",
-        pay: "",
-        ville: "",
+        adresse: "",
         zipcode: 0,
-        rue: "",
+
         specialitee: "",
         type: ""
+      },
+      ItemObjectRules: {
+        textInput: [
+          v =>
+            (v && v.length >= 3) ||
+            "Text field value must be great than 3 characters"
+        ],
+        emailInput: [v => /.+@.+\..+/.test(v) || "E-mail must be valid"],
+        phoneInput: [
+          v => (v && v.length >= 9) || "Phone number is not valid !"
+        ],
+        requiredField: [v => !!v || "Text field is required !"],
+        adresseInput: [v => (v && v.length >= 6) || "Address is not valid !"]
       },
       //end add item
 
@@ -233,10 +286,9 @@ export default {
           email: "bcskill.safi@gmail.com",
 
           telephone: "0514357890",
-          pay: "Morocco",
-          ville: "Safi",
+
           zipcode: 46000,
-          rue: "Sidi Bouzid",
+          adresse: "Sidi Bouzid",
           specialitee: "Informatique",
           type: "Privee"
         },
@@ -245,10 +297,9 @@ export default {
           email: "ocp.safi@gmail.com",
 
           telephone: "0516857890",
-          pay: "Morocco",
-          ville: "Safi",
+
           zipcode: 46000,
-          rue: "Sidi Bouzid",
+          adresse: "Sidi Bouzid",
 
           specialitee: "Industrielle",
           type: "Half-public"
@@ -259,10 +310,8 @@ export default {
 
           telephone: "0516852390",
 
-          pay: "Morocco",
-          ville: "Safi",
           zipcode: 46000,
-          rue: "Sidi Bouzid",
+          adresse: "Sidi Bouzid",
           specialitee: "Industrielle",
           type: "Half-public"
         },
@@ -271,10 +320,9 @@ export default {
           email: "renaut.tanger@gmail.com",
 
           telephone: "05111122390",
-          pay: "Morocco",
-          ville: "Safi",
+
           zipcode: 46000,
-          rue: "Sidi Bouzid",
+          adresse: "Sidi Bouzid",
 
           specialitee: "Industrielle",
           type: "public"
@@ -303,11 +351,19 @@ export default {
 
     //add item form
     onAddItem() {
-      this.addItemDialog = true;
+      this.onEdit=false;
+      this.itemDialog = true;
     },
-    submit() {},
+    submit() {
+      if (this.$refs.itemForm.validate()) {
+        console.log("Valid !");
+      } else {
+        return;
+      }
+    },
     reset() {
-      this.addItemDialog = false;
+      this.$refs.itemForm.reset();
+      this.itemDialog = false;
     },
     //card actions
 
@@ -315,11 +371,31 @@ export default {
       console.log(item);
     },
     editItem(item) {
-      console.log(item);
+      this.itemDialog=true;
+      this.onEdit=true;
+      //assign object
+      this.ItemObject.nom=item.nom;
+      this.ItemObject.email=item.email;
+      this.ItemObject.telephone=item.telephone;
+      this.ItemObject.adresse=item.adresse;
+      this.ItemObject.zipcode=item.zipcode;
+      this.ItemObject.specialitee=item.specialitee;
+      this.ItemObject.type=item.type;
+      
+      
     },
     deleteItem(item) {
       console.log(item);
     }
-  }
+  },
+  watch: {
+    onEdit:function(v){
+      if(v==true){
+        this.itemFormTitle="Editer des informations"
+      }else{
+        this.itemFormTitle="Ajouter Une Entreprise"
+      }
+    }
+  },
 };
 </script>
