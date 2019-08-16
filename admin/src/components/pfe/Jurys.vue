ebeac919bb4bebc008106bf799e41cc7d926d794
Anonymous <anonymous@localhost.localdomain>
2019-08-16 17:44:56 +0100

    working at aading jury group

--- admin/src/components/pfe/Jurys.vue
+++ admin/src/components/pfe/Jurys.vue
@@ -9,20 +9,18 @@
         item-value="value"
       ></v-select>
       <v-spacer></v-spacer>
-      <v-btn color="secondary">Ajouter Un Group</v-btn>
+      <v-btn color="secondary" @click="onAddGrp">Ajouter Un Group</v-btn>
     </v-layout>
     <v-card
       class="pa-2 mb-2 pl-5"
-      v-for="item in jurys"
-      :key="item.number"
+      v-for="(item,index) in jurys"
+      :key="index"
       v-ripple="{ class: `indigo--text lighten-2` }"
     >
       <v-layout row wrap class="pa-3">
         <v-flex xs12 sm8 md9>
-          <div class="caption grey--text">Numero de group</div>
-          <div class="body-2 font-weight-bold">
-            Groupe N : {{ item.number }}
-          </div>
+          <div class="caption grey--text">Numero de Groupe</div>
+          <div class="body-2 font-weight-bold">Groupe N : {{ index+1}}</div>
         </v-flex>
 
         <!-- operations -->
@@ -41,12 +39,7 @@
         <v-flex xs12 sm12 md12 lg12>
           <div class="caption grey--text">Membres de groupe</div>
           <div>
-            <v-layout
-              row
-              wrap
-              v-for="member in item.members"
-              :key="member.email"
-            >
+            <v-layout row wrap v-for="member in item.members" :key="member.email">
               <v-flex xs4 sm4 md4 lg4>
                 <ul>
                   <li>{{ member.name }}</li>
@@ -67,6 +60,113 @@
         </v-flex>
       </v-layout>
     </v-card>
+
+    <div>
+      <!-- modals  -->
+      <!--  add group  -->
+      <v-layout justify-center>
+        <v-dialog v-model="addItemDialog" persistent max-width="590">
+          <v-card>
+            <v-card-title class="headline">Ajouter Un Groupe</v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+              <v-container grid-list-md>
+                <v-form ref="addItemForm" v-model="addItemValidForm" lazy-validation>
+                  <v-select
+                    label="Selectioner le departement"
+                    :items="deps"
+                    v-model="GroupObject.departement"
+                    :rules="GroupObjectRules.requiredField"
+                    item-text="name"
+                    item-value="value"
+                  ></v-select>
+
+                  <!-- ########### -->
+                  <v-card class="pa-5 mb-2" v-for="(item,index) in GroupObject.members" :key="index">
+                    <div class="caption grey--text">
+                      Membre :
+                      <span class="body-2 font-weight-bold">{{index+1}}</span>
+                    </div>
+
+                    <v-text-field
+                      type="text"
+                      label="Nom"
+                      :rules="GroupObjectRules.textInput"
+                      v-model="item.name"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      type="email"
+                      label="Email"
+                      :rules="GroupObjectRules.emailInput"
+                      v-model="item.email"
+                      required
+                    ></v-text-field>
+
+                    <v-text-field
+                      type="phone"
+                      label="Telephone"
+                      :rules="GroupObjectRules.phoneInput"
+                      v-model="item.phone"
+                      required
+                    ></v-text-field>
+                    <v-layout justify-end>
+                      <v-btn fab icon small @click="onRemoveMember(index)">
+                        <v-icon>remove</v-icon>
+                      </v-btn>
+                      <v-btn fab icon small @click="onAddMember">
+                        <v-icon>add</v-icon>
+                      </v-btn>
+                      
+                    </v-layout>
+                  </v-card>
+                  
+                </v-form>
+              </v-container>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="indigo darken-1" text @click="addGrpreset">Annuler</v-btn>
+              <v-btn color="indigo darken-1" text @click="addGrpsubmit">Enregistrer</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-layout>
+    </div>
+    <!--  
+      <v-card class="pa-5 mb-2" v-for="index in  membersArray" :key="index">
+                    <div class="caption grey--text">
+                      Membre :
+                      <span class="body-2 font-weight-bold">{{index+1}}</span>
+                    </div>
+
+                    <v-text-field
+                      type="text"
+                      label="Nom"
+                      :rules="GroupObjectRules.textInput"
+                      v-model="GroupObject.members.name"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      type="email"
+                      label="Email"
+                      :rules="GroupObjectRules.emailInput"
+                      v-model="GroupObject.members.email"
+                      required
+                    ></v-text-field>
+
+                    <v-text-field
+                      type="phone"
+                      label="Telephone"
+                      :rules="GroupObjectRules.phoneInput"
+                      v-model="GroupObject.members.phone"
+                      required
+                    ></v-text-field>
+                  </v-card>
+                  
+     -->
+    
   </div>
 </template>
 
@@ -86,10 +186,8 @@ export default {
           value: "indusGpmc"
         }
       ],
-      //jurys data
       jurys: [
         {
-          number: 1,
           members: [
             {
               name: "Atlas Abdelghafour",
@@ -110,7 +208,6 @@ export default {
           departement: "infoGtr"
         },
         {
-          number: 2,
           members: [
             {
               name: "Boukhatem Ali",
@@ -130,8 +227,74 @@ export default {
           ],
           departement: "indusGpmc"
         }
-      ]
+      ],
+      //add group
+      addItemValidForm: false,
+      addItemDialog: false,
+      membresNumber: 1,
+
+      GroupObject: {
+        departement: "",
+        members: [
+          {
+            name: "",
+            email: "",
+            phone: ""
+          }
+        ]
+      },
+      GroupObjectRules: {
+        textInput: [
+          v =>
+            (v && v.length >= 3) ||
+            "Text field value must be great than 3 characters"
+        ],
+        emailInput: [v => /.+@.+\..+/.test(v) || "E-mail must be valid"],
+        phoneInput: [
+          v => (v && v.length >= 9) || "Phone number is not valid !"
+        ],
+        requiredField: [v => !!v || "Text field is required !"]
+      }
     };
+  },
+  created() {},
+  methods: {
+    initialize() {},
+    onAddGrp() {
+      this.addItemDialog = true;
+    },
+    onAddMember(){
+      
+      this.GroupObject.members.push({
+        name:"",
+        email:"",
+        phone:""
+      });
+    },
+    onRemoveMember(index){
+      if(index>0){
+        
+        this.GroupObject.members.splice(index, 1);
+      }
+    },
+    addGrpreset() {
+      this.$refs.addItemForm.reset();
+      this.addItemDialog = false;
+    },
+    addGrpsubmit() {
+      if (this.$refs.addItemForm.validate()) {
+        console.log(this.GroupObject);
+      }
+    }
+  },
+  computed: {
+    membersArray: function() {
+      let arr = [];
+      for (let i = 0; i < this.membresNumber; i++) {
+        arr.push(i);
+      }
+      return arr;
+    }
   }
 };
 </script>
